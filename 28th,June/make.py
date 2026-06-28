491a77a33438f85cad91dab104b11c7ca828e2c6
HyunjunJu <hyunjun.ju16@gmail.com>
2026-06-28 11:00:03 +0900

    Make: NieR script from scratch

---
+++ 28th,June/make.py
@@ -0,0 +1,7 @@
+title = "NieR: Automata"
+released = 2010
+age = 2026 - released
+
+print(f"One of my favorite games is {title}. It's been {age} years since it's release.")
+print(title.upper())
+print("=" * len(title))
