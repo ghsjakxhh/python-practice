3ac868a4310f2570f5bc7435dbd735d66cf92a9b
HyunjunJu <hyunjun.ju16@gmail.com>
2026-07-01 23:53:42 +0900

    Session 5: three-phase summer countdown

---
+++ 1st,July/make5.py
@@ -0,0 +1,15 @@
+days_left = 60
+
+while days_left > 30:
+    print(f"{days_left} days of summer break. Cortisol level low")
+    days_left = days_left - 1
+
+while days_left > 10:
+    print(f"{days_left} days of summer break. Jesus Christ. Time flies.")
+    days_left = days_left - 1
+
+while days_left > 0:
+    print(f"{days_left} days until hell breaks loose. Massive cortisol spike!")
+    days_left = days_left - 1
+
+print("Go to school!!!")
---
+++ 1st,July/session5.py
@@ -0,0 +1,7 @@
+reps_left = 6
+
+while reps_left > 0:
+    print(f"Sprint! {reps_left} reps to go.")
+    
+
+print("Done. Go home.")
