097142000d9713c1e2de05f0bc4ebcb51f838edf
HyunjunJu <hyunjun.ju16@gmail.com>
2026-07-02 11:57:19 +0900

    Brushing up on session 1

---
+++ 2nd,July/make1.py
@@ -0,0 +1,7 @@
+video_game = "Silent Hill 2"
+released = 1990
+age = 2026 - released
+
+print(f"{video_game} was released in {released}. That make the game {age} years old.")
+print(video_game.upper())
+print(len(video_game))
